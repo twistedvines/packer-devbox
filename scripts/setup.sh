900f02d2d33e8115ae61a71597b275b685a7e400
Harry Owen <harry@twistedvin.es>
2017-10-09 13:23:37 +0100

    Use group `src` to manage `/usr/local/src`

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,14 @@ echo "%wheel      ALL=(ALL) ALL" | tee -a /etc/sudoers.d/wheel > /dev/null
 
 # add my user
 groupadd hobag
-useradd -m -g hobag -G wheel -s /bin/bash hobag
+
+# add the src group for access to /usr/local/src
+groupadd src
+
+chown -R ':src' '/usr/local/src'
+chmod 0775 '/usr/local/src'
+
+useradd -m -g hobag -G wheel,src -s /bin/bash hobag
 
 user_password="$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c16)"
 echo "hobag:${user_password}" | chpasswd
