c71c30885531e755f9cccae7666970b3687339fe
Harry Owen <harry@twistedvin.es>
2017-11-05 23:01:55 +0000

    Use arch-devbox-install submodule

--- files/.bash_profile
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# basic .bash_profile. Runs on spawn of login shell.
-
-if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
-  exec startx
-else
-  [ -f ~/.bashrc ] && source ~/.bashrc
-fi
--- scripts/tidy-up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script tidies up after the installation process.
-
-# revoke hobag's nopasswd group membership
-gpasswd -d hobag nopasswd
-
-# add hobag to wheel group for password sudo
-usermod -a -G wheel hobag
